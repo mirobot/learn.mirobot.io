11553dbb0575309754a472347c47e3a411feca2f
Ben Pirt <ben@pirt.co.uk>
2015-05-14 12:58:11 +0100

    Enable tag-based filtering

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,21 @@
 var hidden = [];
 var visible = [];
+var tags = [];
 
 var docHidden = function(doc){
-  
+  if(tags.length > 0){
+    var docTags = doc.getAttribute("data-tags").split('|');
+    var hide = true;
+    for(var tag in tags){
+      if(docTags.indexOf(tags[tag]) > -1){
+        console.log('found tag');
+        hide = false;
+      }
+    }
+    if(hide){
+      return true;
+    }
+  }
   for(var d in hidden){
     if(doc.className.indexOf(hidden[d]) > -1){
       return true;
@@ -22,7 +35,7 @@ var filterDocs = function(){
   }
 }
 
-var clickHandler = function(e){
+var enumFilter = function(e){
   if(this.className == "selected"){
     var i = visible.indexOf(this.getAttribute("data-filter"));
     if(i != -1) { visible.splice(i, 1); }
@@ -38,18 +51,55 @@ var clickHandler = function(e){
   return false;
 }
 
-var setupFilter = function(type){
+var tagFilter = function(e){
+  var siblings = this.parentElement.getElementsByTagName('a');
+  var first_click = true;
+  for(var i=0; i< siblings.length; i++){
+    if(siblings[i].className !== 'selected'){
+      first_click = false;
+      break;
+    }
+  }
+  if(first_click){
+    // just select this tag and deselect the others
+    for(var i=0; i< siblings.length; i++){
+      siblings[i].className = 'unselected';
+    }
+    this.className = 'selected';
+    tags.push(this.getAttribute("data-tag"));
+  }else{
+    // toggle this tag
+    if(this.className == "selected"){
+      var i = tags.indexOf(this.getAttribute("data-tag"));
+      if(i != -1) { tags.splice(i, 1); }
+      this.className = "unselected";
+    }else{
+      tags.push(this.getAttribute("data-tag"));
+      this.className = "selected";
+    }
+  }
+  if(tags.length === 0){
+    for(var i=0; i< siblings.length; i++){
+      siblings[i].className = 'selected';
+    }
+  }
+  filterDocs();
+  return false;
+}
+
+var setupFilter = function(type, fn){
   var links = document.querySelectorAll("#" + type + " a");
   for(var i=0; i<links.length; i++){
     visible.push(links[i].getAttribute("data-filter"));
-    links[i].onclick = clickHandler;
+    links[i].onclick = fn;
     links[i].className = "selected";
   }
 }
 
 var setupFilters = function(){
-  setupFilter('type-filter');
-  setupFilter('level-filter');
+  setupFilter('type-filter', enumFilter);
+  setupFilter('level-filter', enumFilter);
+  setupFilter('tag-filter', tagFilter);
   //setupFilter('hw-filter');
 }
 
