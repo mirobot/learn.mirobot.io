4cc34b382cd201e2aa85cd76efa76dbcc970d3f1
Ben Pirt <ben@pirt.co.uk>
2015-11-23 16:27:10 +0000

    Enable turn calibration

--- assets/docs/calibrating-your-mirobot/cal.js
+++ assets/docs/calibrating-your-mirobot/cal.js
@@ -8,17 +8,19 @@ function Calibrator(){
   this.setupInit();
   this.setupSlack();
   this.setupDistance();
+  this.setupTurn();
 }
 
 Calibrator.prototype = {
   connected: false,
   slackCal: 0,
   moveCal: 1,
+  turnCal: 1,
   setupInit: function(){
     var self = this;
-    this.connectButton = document.querySelector('#init #connect')
-    this.connectButton.addEventListener('click', function(){ self.connect(); });
+    click('#init #connect', function(){ self.connect(); });
   },
+
   setupSlack: function(){
     var self = this;
     click('#slack #start', function(){ self.setSlackCal(self.slackCal); });
@@ -39,10 +41,11 @@ Calibrator.prototype = {
       document.querySelector('#slack .steps').innerHTML = self.slackCal;
     });
   },
+
   setupDistance: function(){
     var self = this;
     click('#distance #draw', function(){ self.distanceDraw(); });
-    click('#distance #calibrate', function(){ self.distanceCal(); });
+    click('#distance #calibrate', function(){ self.distanceCalibrate(); });
   },
   distanceDraw: function(){
     var self=this;
@@ -66,14 +69,15 @@ Calibrator.prototype = {
       self.moveCal = Number(msg.msg);
     });
   },
-  distanceCal: function(){
+  distanceCalibrate: function(){
+    var self = this;
     var desired = Number(document.querySelector('#distance .desired').value);
     var actual = Number(document.querySelector('#distance .actual').value);
     var correctedActual = actual / this.moveCal;
     var newCal = desired / correctedActual;
-    this.mirobot.calibrateMove(newCal, function(e){
+    self.mirobot.calibrateMove(newCal, function(e){
       if(e === 'complete'){
-        this.mirobot.calibrateTurn(newCal, function(e){
+        self.mirobot.calibrateTurn(newCal, function(e){
           if(e === 'complete'){
             document.querySelector('#distance .stepthree').style.display = 'none';
             document.querySelector('#distance .stepfour').style.display = '';
@@ -82,7 +86,54 @@ Calibrator.prototype = {
       }
     })
   },
+
+  setupTurn: function(){
+    var self = this;
+    click('#turn #draw', function(){ self.turnDraw(); });
+    click('#turn #calibrate', function(){ self.turnCalibrate(); });
+  },
+  turnDraw: function(){
+    var self=this;
+    document.querySelector('#turn .stepone').style.display = 'none';
+    document.querySelector('#turn .steptwo').style.display = '';
+    self.mirobot.pendown(function(e){
+      if(e === 'complete'){
+        self.mirobot.forward(10, function(e){
+          if(e === 'complete'){
+            self.mirobot.right(36, function(e){
+              if(e === 'complete'){
+                self.mirobot.forward(10, function(e){
+                  if(e === 'complete'){
+                    document.querySelector('#turn .steptwo').style.display = 'none';
+                    document.querySelector('#turn .stepthree').style.display = '';
+                  }
+                });
+              }
+            });
+          }
+        });
+      }
+    });
+  },
+  getTurnState: function(){
+    var self = this;
+    this.mirobot.turnCalibration(function(e, msg){
+      self.turnCal = Number(msg.msg);
+    });
+  },
+  turnCalibrate: function(){
+    var diff = Number(document.querySelector('#turn .diff').value);
+    var newCal = ((360 + diff) / 360) * this.turnCal;
+    this.mirobot.calibrateTurn(newCal, function(e){
+      if(e === 'complete'){
+        document.querySelector('#distance .stepthree').style.display = 'none';
+        document.querySelector('#distance .stepfour').style.display = '';
+      }
+    });
+  },
+
   connect: function(){
+    if(this.connected) return;
     var self = this;
     var address = document.querySelector('#address').value;
     if(address === ''){
@@ -98,6 +149,7 @@ Calibrator.prototype = {
       this.updateConnectionState();
       this.getSlackState();
       this.getDistanceState();
+      this.getTurnState();
       this.mirobot.version(function(e, msg){
         if(/^\d+\.\d+\.\d+$/.test(msg.msg)){
           if(msg.msg < '2.0.4'){
